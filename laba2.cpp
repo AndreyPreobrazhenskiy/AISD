74aa93b2a35a59a244ad791bcc1d4701e3458e6d
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2024-11-25 01:30:27 +0400

    +hanoi

--- laba2.cpp
+++ laba2.cpp
@@ -15,3 +15,19 @@ bool is_happy(int number) {
     }
     return number == 1;
 }
+
+void solve_hanoi(int n, LinkedList<int>& source, LinkedList<int>& target, LinkedList<int>& auxiliary) {
+    if (n == 1) {
+        int disk = source[source.get_size() - 1];
+        source.pop_tail();
+        target.push_tail(disk);
+        std::cout << "Moved disk " << disk << " from Source to Target\n";
+        return;
+    }
+    solve_hanoi(n - 1, source, auxiliary, target);
+    int disk = source[source.get_size() - 1];
+    source.pop_tail();
+    target.push_tail(disk);
+    std::cout << "Moved disk " << disk << " from Source to Target\n";
+    solve_hanoi(n - 1, auxiliary, target, source);
+}
