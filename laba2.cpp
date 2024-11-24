e305dc9726d75ef62113297ed276236a26a3d805
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2024-11-25 02:19:13 +0400

    +main

--- laba2.cpp
+++ laba2.cpp
@@ -31,3 +31,46 @@ void solve_hanoi(int n, LinkedList<int>& source, LinkedList<int>& target, Linked
     std::cout << "Moved disk " << disk << " from Source to Target\n";
     solve_hanoi(n - 1, auxiliary, target, source);
 }
+
+int main() {
+    // Пример работы всех методов
+    LinkedList<int> list;
+    list.push_tail(1);
+    list.push_tail(2);
+    list.push_head(0);
+    std::cout << "List: " << list << std::endl;
+
+    LinkedList<int> another_list(5, 10, 50);
+    std::cout << "Another list: " << another_list << std::endl;
+
+    list.push_tail(another_list);
+    std::cout << "Merged list: " << list << std::endl;
+
+    list.pop_head();
+    list.pop_tail();
+    std::cout << "After pops: " << list << std::endl;
+
+    list.delete_node(2);
+    std::cout << "After deleting 2s: " << list << std::endl;
+
+    // Счастливые числа
+    LinkedList<int> happy_list(20, 1, 100);
+    std::cout << "---------------------------------------------";
+    std::cout << "\nHappy numbers: ";
+    for (size_t i = 0; i < happy_list.get_size(); ++i) {
+        if (is_happy(happy_list[i])) {
+            std::cout << happy_list[i] << " ";
+        }
+    }
+    std::cout << std::endl;
+    std::cout << "---------------------------------------------\n";
+
+    // Ханойская башня
+    LinkedList<int> tower1, tower2, tower3;
+    for (int i = 3; i > 0; --i) {
+        tower1.push_tail(i);
+    }
+    solve_hanoi(3, tower1, tower2, tower3);
+
+    return 0;
+}
