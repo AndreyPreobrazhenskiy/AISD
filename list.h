7b8f9b4e450ed2f2a1d807221fb8646489069e60
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2024-11-24 22:07:23 +0400

    +class

--- list.h
+++ list.h
@@ -9,3 +9,26 @@ struct Node {
 
     Node(T value) : data(value), next(nullptr) {}
 };
+
+
+template <typename T>
+class LinkedList {
+private:
+    Node<T>* head;
+    Node<T>* tail;
+    size_t size;
+
+    void copy_list(const LinkedList& other) {
+        Node<T>* current = other.head;
+        while (current) {
+            push_tail(current->data);
+            current = current->next;
+        }
+    }
+
+    void clear_list() {
+        while (head) {
+            pop_head();
+        }
+    }
+};
