1ec9fc602d5493d87b6c8843eca51ae8d40491e4
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2025-03-16 01:56:34 +0400

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,53 @@
+#include "lab1.h"
+#include <vector>
+#include <chrono>
+#include <iostream>
+
+
+size_t lcg() {
+    static size_t x = 0;
+    x = (1021 * x + 24631) % 116640;
+    return x;
+}
+
+template <typename Container>
+void testInsert(Container& container, size_t num_elements) {
+    for (size_t i = 0; i < num_elements; ++i) {
+        container.insert(lcg());
+    }
+}
+
+template <typename Container>
+void testFind(Container& container, size_t num_attempts) {
+    for (size_t i = 0; i < num_attempts; ++i) {
+        container.contains(lcg());
+    }
+}
+
+template <typename Container>
+void testErase(Container& container, size_t num_attempts) {
+    for (size_t i = 0; i < num_attempts; ++i) {
+        container.erase(lcg());
+    }
+}
+
+void testVectorInsert(std::vector<int>& vec, size_t num_elements) {
+    for (size_t i = 0; i < num_elements; ++i) {
+        vec.push_back(lcg());
+    }
+}
+
+void testVectorFind(const std::vector<int>& vec, size_t num_attempts) {
+    for (size_t i = 0; i < num_attempts; ++i) {
+        auto it = std::find(vec.begin(), vec.end(), lcg());
+    }
+}
+
+void testVectorErase(std::vector<int>& vec, size_t num_attempts) {
+    for (size_t i = 0; i < num_attempts; ++i) {
+        auto it = std::find(vec.begin(), vec.end(), lcg());
+        if (it != vec.end()) {
+            vec.erase(it);
+        }
+    }
+}
